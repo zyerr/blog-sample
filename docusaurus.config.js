563d9337ec1ddb2c459065099dc54ce978e5dca5
zyerr <zy_20160828@163.com>
2021-12-07 00:07:55 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,7 +49,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         items: [
           {
             type: 'doc',
-            docId: 'Intro',
+            docId: 'intro',
             position: 'left',
             label: 'Docs',
           },
